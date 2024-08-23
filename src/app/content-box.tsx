9af78ce5bafeb19270da2b5843a49bb4fe9d4fcc
z0xm <mukulz2020@gmail.com>
2024-08-23 19:26:43 +0530

    init

---
+++ src/app/content-box.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import * as React from 'react';
+
+import { Button } from '@/components/ui/button';
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible';
+
+export function ContentBox(props: { level: number; index: number; title: React.ReactNode; content: React.ReactNode }) {
+	const [isOpen, setIsOpen] = React.useState(false);
+	const code = `${props.level}${props.index}`;
+	return (
+		<Collapsible open={isOpen} onOpenChange={setIsOpen} className='max-w-screen w-fit'>
+			<CollapsibleTrigger asChild>
+				<div className='cursor-pointer font-semibold underline italic'>
+					{props.title} <sup>[{code}]</sup>
+				</div>
+			</CollapsibleTrigger>
+			<CollapsibleContent>
+				<div className='border-white border-[1px] rounded-md p-2'>
+					<sup>[{code}] </sup>
+					{props.content}
+				</div>
+			</CollapsibleContent>
+		</Collapsible>
+	);
+}
